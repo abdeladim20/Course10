8d3aacc803546beec092b8c777733b43cba6afca
abdeladim20 <linuxacount1@gmail.com>
2025-09-15 12:28:58 +0100

    using set and get

---
+++ SetAndGet.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class clsPerson
+{
+private:
+    string _FirstName;
+    string _LastName;
+
+public:
+    // Property Set
+    void setFirstName(string FirstName) { _FirstName = FirstName; }
+
+    // Property Get
+    string FirstName() { return _FirstName; }
+
+    // Property Set
+    void setLastName(string LastName) { _LastName = LastName; }
+
+    // Property Get
+    string LastName() { return _LastName; }
+    string FullName() { return _FirstName + " " + _LastName; }
+};
+
+int main()
+{
+    clsPerson Person1;
+    Person1.setFirstName("Abdeladim");
+    Person1.setLastName("Jabbouri");
+    cout << "First Name:" << Person1.FirstName() << endl;
+    cout << "Last Name:" << Person1.LastName() << endl;
+    cout << "Full Name:" << Person1.FullName() << endl;
+    return 0;
+}
