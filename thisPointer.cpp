27c492abc1e2120b67e1256cdd7cfd21301312b7
abdeladim20 <linuxacount1@gmail.com>
2025-11-08 23:21:35 +0100

    this in cpp && create string class

---
+++ thisPointer.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class clsEmployee
+{
+private:
+    int ID;
+    string Name;
+    float Salary;
+
+public:
+    clsEmployee(int ID, string Name, float Salary)
+    {
+        this->ID = ID;
+        this->Name = Name;
+        this->Salary = Salary;
+    }
+
+    static void Func1(clsEmployee Employee) { Employee.Print(); }
+
+    void Func2()
+    {
+        Func1(*this);
+    }
+
+    void Print()
+    {
+        cout << ID << "  " << Name << "  " << Salary << endl;
+        // cout << this->ID << "  " << this->Name << "  " << this->Salary << endl;
+    }
+};
+
+int main()
+{
+    clsEmployee Employee1(100, "Abdeladim", 5000);
+    Employee1.Print();
+    Employee1.Func2();
+    return 0;
+}
